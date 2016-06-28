f1ea2ae9d58d4d52d3e3c10d8760d122d870e85d
cleberson <clebrsonn@hotmail.com>
2016-06-27 23:54:45 -0300

    listar todas as categorias

--- src/main/java/com/teccsoluction/sushi/CategoriaController.java
+++ src/main/java/com/teccsoluction/sushi/CategoriaController.java
@@ -1,9 +1,7 @@
 package com.teccsoluction.sushi;
 
-import java.util.List;
-
-import javax.servlet.http.HttpServletRequest;
-
+import com.teccsoluction.sushi.dao.CategoriaDAO;
+import com.teccsoluction.sushi.entidade.Categoria;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -11,42 +9,49 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.teccsoluction.sushi.dao.CategoriaDAO;
-import com.teccsoluction.sushi.entidade.Categoria;
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 /**
  * Handles requests for the application home page.
  */
 @Controller
 public class CategoriaController {
-	
 
-	@Autowired
-	private CategoriaDAO categoriaDao;
-	
-	private List<Categoria> categoriaList;
-	
-	
-	
-	
-	@RequestMapping(value = "/cadastrocategoria", method = RequestMethod.GET)
-	public ModelAndView cadastrocategoria() {
-		
-		ModelAndView cadastrocategoria = new ModelAndView("cadastrocategoria");
-		
-		return cadastrocategoria;
-	}
-	
-	@RequestMapping(value = "/AddCategoria", method = RequestMethod.POST)
-	public ModelAndView AdicionarCategoria(@ModelAttribute("Categoria") Categoria categoria) {
-		
-		ModelAndView cadastro_categoria = new ModelAndView("cadastrocategoria");
-		
-		categoriaDao.add(categoria);
-		
-		return cadastro_categoria;
-	}
-//	
+
+    @Autowired
+    private CategoriaDAO categoriaDao;
+
+    private List<Categoria> categoriaList;
+
+
+    @RequestMapping(value = "/cadastrocategoria", method = RequestMethod.GET)
+    public ModelAndView cadastrocategoria() {
+
+        ModelAndView cadastrocategoria = new ModelAndView("cadastrocategoria");
+
+        categoriaList = categoriaDao.getAll();
+
+        cadastrocategoria.addObject("categoriaList", categoriaList);
+
+
+        return cadastrocategoria;
+    }
+
+    @RequestMapping(value = "/AddCategoria", method = RequestMethod.GET)
+    public ModelAndView AdicionarCategoria(@ModelAttribute("Categoria") Categoria categoria) {
+
+//        long idf = Long.parseLong(request.getParameter("catPai"));
+
+//        Categoria catPai = categoriaDao.PegarPorId(idf);
+        ModelAndView cadastro_categoria = new ModelAndView("cadastrocategoria");
+//        categoria.setCatPai(catPai);
+        categoriaDao.add(categoria);
+
+        return cadastro_categoria;
+    }
+
+    //
 //	@RequestMapping(value = "cadastrocategoria", method = RequestMethod.GET)
 //	public ModelAndView cadastrocategoria() {
 //		
@@ -55,58 +60,55 @@ public class CategoriaController {
 //		return cadastrocategoria;
 //	}
 //	
-	@RequestMapping(value = "/movimentacaocategoria", method = RequestMethod.GET)
-	public ModelAndView movimentacaoCategoria() {
-		
-		ModelAndView movimentacaocategoria = new ModelAndView("movimentacaocategoria");
-		
-		categoriaList = categoriaDao.getAll();
-		movimentacaocategoria.addObject("categoriaList", categoriaList);
-		
-		
-		return movimentacaocategoria;
-	}
-	
-	@RequestMapping(value = "edicaocategoria", method = RequestMethod.GET)
-	public ModelAndView editarCategoriaForm(HttpServletRequest request) {
-		
-		Categoria categoria;
-		long idf = Long.parseLong(request.getParameter("id"));
-		ModelAndView edicaocategoria = new ModelAndView("edicaocategoria");
-		categoria = categoriaDao.PegarPorId(idf);
-		edicaocategoria.addObject("categoria", categoria);
-		
-		
-		return edicaocategoria;
-	}
-	
-	
-	@RequestMapping(value = "/movimentacaocategoria/editarCategoria", method = RequestMethod.GET)
-	public ModelAndView editarCategoria(HttpServletRequest request, Categoria categoria) {
-		
-		
-		
-		Long idf = Long.parseLong(request.getParameter("id"));
-		
-		categoriaDao.editar(idf, categoria);
-		
-		
-		
-		return new ModelAndView("redirect:/movimentacaocategoria");
-	}
-	
-	
-	
-	@RequestMapping(value = "/movimentacaocategoria/delete", method = RequestMethod.GET)
-	public ModelAndView deleteCategoria(HttpServletRequest request) {
-		
-		
-		long idf = Long.parseLong(request.getParameter("id"));
+    @RequestMapping(value = "/movimentacaocategoria", method = RequestMethod.GET)
+    public ModelAndView movimentacaoCategoria() {
+
+        ModelAndView movimentacaocategoria = new ModelAndView("movimentacaocategoria");
+
+        categoriaList = categoriaDao.getAll();
+        movimentacaocategoria.addObject("categoriaList", categoriaList);
+
+
+        return movimentacaocategoria;
+    }
+
+    @RequestMapping(value = "edicaocategoria", method = RequestMethod.GET)
+    public ModelAndView editarCategoriaForm(HttpServletRequest request) {
+
+        Categoria categoria;
+        long idf = Long.parseLong(request.getParameter("id"));
+        ModelAndView edicaocategoria = new ModelAndView("edicaocategoria");
+        categoria = categoriaDao.PegarPorId(idf);
+        edicaocategoria.addObject("categoria", categoria);
+
+
+        return edicaocategoria;
+    }
+
+
+    @RequestMapping(value = "/movimentacaocategoria/editarCategoria", method = RequestMethod.GET)
+    public ModelAndView editarCategoria(HttpServletRequest request, Categoria categoria) {
+
+
+        Long idf = Long.parseLong(request.getParameter("id"));
+
+        categoriaDao.editar(idf, categoria);
+
+
+        return new ModelAndView("redirect:/movimentacaocategoria");
+    }
+
+
+    @RequestMapping(value = "/movimentacaocategoria/delete", method = RequestMethod.GET)
+    public ModelAndView deleteCategoria(HttpServletRequest request) {
+
+
+        long idf = Long.parseLong(request.getParameter("id"));
 //		ModelAndView movimentacaocategoria = new ModelAndView("movimentacaocategoria");
-		categoriaDao.delete(idf);
-		
-		
-		return new ModelAndView("redirect:/movimentacaocategoria");
-	}
-	
+        categoriaDao.delete(idf);
+
+
+        return new ModelAndView("redirect:/movimentacaocategoria");
+    }
+
 }
--- src/main/webapp/WEB-INF/views/jsp/cadastro/cadastrocategoria.jsp
+++ src/main/webapp/WEB-INF/views/jsp/cadastro/cadastrocategoria.jsp
@@ -1,40 +1,43 @@
-<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
-<%@ page session="false"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ page session="false" %>
 
 <div id="content" class="content">
 
-		<h3>Cadastro de Categorias</h3>
-	
-	
-	<div id="form">
-	<form id="formCategoria" action="AddCategoria" method="POST">
+    <h3>Cadastro de Categorias</h3>
 
 
-
-		<p>ID da Categoria:</p>
-		<input name="id" type="text" class="inp-form" readonly="readonly" />
-
-
-		<p>Nome da Categoria:</p>
-		<input name="nome"type="text" class="inp-form" />
+    <div id="form">
+        <form id="formCategoria" action="AddCategoria" method="GET">
 
 
+            <p>ID da Categoria:</p>
+            <input name="id" type="text" class="inp-form" readonly="readonly"/>
+
+            <p>Categoria Pai:</p>
+            <select id="catPai" name="catPai" id="files" class="inp-form">
+                <optgroup label="Categoria Pai">
+                     			<c:forEach var="categoria" items="${categoriaList}">
+                     				<option value="${categoria}">${categoria.nome}</option>
+                     				</c:forEach>
+
+                </optgroup>
+            </select>
 
 
-
-		<div id="botoes">
-				
-				<button class="button">Adicionar</button>
-				<button class="button">Apagar</button>
-				
-		
-		</div>
-		
-			</form>
-		</div>
+            <p>Nome da Categoria:</p>
+            <input name="nome" type="text" class="inp-form"/>
 
 
+            <div id="botoes">
 
+                <button class="button">Adicionar</button>
+                <button class="button">Apagar</button>
+
+
+            </div>
+
+        </form>
+    </div>
 
 
 </div>
