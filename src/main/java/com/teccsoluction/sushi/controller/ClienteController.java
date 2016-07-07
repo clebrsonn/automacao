f871924a4a8863869d78ee696ac660e9b7482fed
cleberson <clebrsonn@hotmail.com>
2016-07-06 23:00:43 -0300

    ClienteController

--- src/main/java/com/teccsoluction/sushi/controller/ClienteController.java
+++ src/main/java/com/teccsoluction/sushi/controller/ClienteController.java
@@ -1,29 +1,29 @@
-package com.teccsoluction.sushi.controller;
-
-import com.teccsoluction.sushi.dao.ClienteDAO2;
-import com.teccsoluction.sushi.entidade.Cliente;
-import com.teccsoluction.sushi.framework.AbstractController;
-import com.teccsoluction.sushi.framework.AbstractEntityDao;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-/**
- * Created by clebr on 06/07/2016.
- */
-@RequestMapping(value = "cliente/")
-public class ClienteController extends AbstractController<Cliente> {
-
-
-    private @Autowired
-    ClienteDAO2 dao;
-
-
-    public ClienteController() {
-        super("Cliente");
-    }
-
-    @Override
-    protected AbstractEntityDao<Cliente> getDao() {
-        return dao;
-    }
-}
+//package com.teccsoluction.sushi.controller;
+//
+//import com.teccsoluction.sushi.dao.ClienteDAO2;
+//import com.teccsoluction.sushi.entidade.Cliente;
+//import com.teccsoluction.sushi.framework.AbstractController;
+//import com.teccsoluction.sushi.framework.AbstractEntityDao;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//
+///**
+// * Created by clebr on 06/07/2016.
+// */
+//@RequestMapping(value = "cliente/")
+//public class ClienteController extends AbstractController<Cliente> {
+//
+//
+//    private @Autowired
+//    ClienteDAO2 dao;
+//
+//
+//    public ClienteController() {
+//        super("Cliente");
+//    }
+//
+//    @Override
+//    protected AbstractEntityDao<Cliente> getDao() {
+//        return dao;
+//    }
+//}
