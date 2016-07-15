7d24c01be0ba10f92eea42de5688880f519ab6c3
cleberson <clebrsonn@hotmail.com>
2016-07-14 22:28:41 -0300

    Categoria

--- src/main/java/com/teccsoluction/sushi/dao/CategoriaDAO.java
+++ src/main/java/com/teccsoluction/sushi/dao/CategoriaDAO.java
@@ -1,64 +1,81 @@
 package com.teccsoluction.sushi.dao;
 
-import java.util.List;
-
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
 import com.teccsoluction.sushi.entidade.Categoria;
+import com.teccsoluction.sushi.framework.AbstractEntityDao;
+import org.springframework.stereotype.Service;
+
+import javax.persistence.TypedQuery;
 
 
 @Service
-public class CategoriaDAO {
-	
-	@PersistenceContext
-	  private EntityManager em;
-	
-	
-	
-	  public List<Categoria> getAll() {
-		    List<Categoria> result = em.createQuery("SELECT p FROM Categoria p", Categoria.class).getResultList();
-		    return result;
-		  }
-		   
-			@Transactional
-		  public void add(Categoria categoria) {			
-		    em.persist(categoria);
-		  }
-			@Transactional
-			  public void delete(long id) {
-			    Categoria categoria = em.getReference(Categoria.class, id);
-				em.remove(categoria);
-				
-			  }
-			
-			@Transactional
-			public Categoria PegarPorId(long id){
-				
-				Categoria categoria = em.find(Categoria.class, id);
-				
-				
-				return categoria;
-			}
-			
-			@Transactional
-			public void editar(long id,Categoria categoria){
-				
-				Categoria categoriaant =em.find(Categoria.class, id);
-				
-				categoriaant.setNome(categoria.getNome());
-				categoriaant.setCatpai(categoria.getCatpai());
-//				categoria.setCatpai(categoriaant.getCatpai());
-				
-				categoria = em.merge(categoriaant);
-				
-				
-				
-			}
-	
+public class CategoriaDAO extends AbstractEntityDao<Categoria>{
+
+
+    public CategoriaDAO() {
+        super(Categoria.class, "Categoria");
+    }
+
+    @Override
+    protected void appendPredicate(StringBuilder predicate) {
+
+    }
+
+    @Override
+    protected void setParameters(TypedQuery<?> query) {
+
+    }
+
+    @Override
+    protected void appendOrder(StringBuilder predicate) {
+
+    }
+
+
+//	@PersistenceContext
+//	  private EntityManager em;
+//
+//
+//
+//	  public List<Categoria> getAll() {
+//		    List<Categoria> result = em.createQuery("SELECT p FROM Categoria p", Categoria.class).getResultList();
+//		    return result;
+//		  }
+//
+//			@Transactional
+//		  public void add(Categoria categoria) {
+//		    em.persist(categoria);
+//		  }
+//			@Transactional
+//			  public void delete(long id) {
+//			    Categoria categoria = em.getReference(Categoria.class, id);
+//				em.remove(categoria);
+//
+//			  }
+//
+//			@Transactional
+//			public Categoria PegarPorId(long id){
+//
+//				Categoria categoria = em.find(Categoria.class, id);
+//
+//
+//				return categoria;
+//			}
+//
+//			@Transactional
+//			public void editar(long id,Categoria categoria){
+//
+//				Categoria categoriaant =em.find(Categoria.class, id);
+//
+//				categoriaant.setNome(categoria.getNome());
+//				categoriaant.setCatpai(categoria.getCatpai());
+////				categoria.setCatpai(categoriaant.getCatpai());
+//
+//				categoria = em.merge(categoriaant);
+//
+//
+//
+//			}
+//
 	
 
 }
