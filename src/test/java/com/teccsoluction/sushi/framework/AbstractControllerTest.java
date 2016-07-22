fe9d38211ea609c7459b6cf3672e3bb3b4ec800d
cleberson <clebrsonn@hotmail.com>
2016-07-21 22:57:23 -0300

    Classes de teste

---
+++ src/test/java/com/teccsoluction/sushi/framework/AbstractControllerTest.java
@@ -0,0 +1,44 @@
+package com.teccsoluction.sushi.framework;
+
+import org.junit.Test;
+
+/**
+ * Created by clebr on 21/07/2016.
+ */
+public class AbstractControllerTest {
+    @Test
+    public void getDao() throws Exception {
+
+    }
+
+    @Test
+    public void cadastrarEntity() throws Exception {
+
+    }
+
+    @Test
+    public void adicionarEntity() throws Exception {
+
+    }
+
+    @Test
+    public void movimentacaoEntity() throws Exception {
+
+    }
+
+    @Test
+    public void editarEntityForm() throws Exception {
+
+    }
+
+    @Test
+    public void editarEntity() throws Exception {
+
+    }
+
+    @Test
+    public void deletarEntity() throws Exception {
+
+    }
+
+}
---
+++ src/test/java/com/teccsoluction/sushi/framework/AbstractEntityDaoTest.java
@@ -0,0 +1,49 @@
+package com.teccsoluction.sushi.framework;
+
+import org.junit.Test;
+
+/**
+ * Created by clebr on 21/07/2016.
+ */
+public class AbstractEntityDaoTest {
+    @Test
+    public void getAll() throws Exception {
+
+    }
+
+    @Test
+    public void add() throws Exception {
+
+    }
+
+    @Test
+    public void appendPredicate() throws Exception {
+
+    }
+
+    @Test
+    public void setParameters() throws Exception {
+
+    }
+
+    @Test
+    public void appendOrder() throws Exception {
+
+    }
+
+    @Test
+    public void editar() throws Exception {
+
+    }
+
+    @Test
+    public void delete() throws Exception {
+
+    }
+
+    @Test
+    public void pegarPorId() throws Exception {
+
+    }
+
+}
