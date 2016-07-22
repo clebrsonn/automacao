965a5b91076058afb6763176d6d57acbb4915291
cleberson <clebrsonn@hotmail.com>
2016-07-21 21:08:31 -0300

    cliente controller funcionando

--- src/main/java/com/teccsoluction/sushi/controller/ClienteController.java
+++ src/main/java/com/teccsoluction/sushi/controller/ClienteController.java
@@ -11,98 +11,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class ClienteController extends AbstractController<Cliente> {
 
-        private @Autowired
-        ClienteDAO2 dao;
+    private
+    final
+    ClienteDAO2 dao;
 
-        public ClienteController() {
-            super("cliente");
-        }
+    @Autowired
+    public ClienteController(ClienteDAO2 dao) {
+        super("cliente");
+        this.dao = dao;
+    }
 
 
-        @Override
-        protected ClienteDAO2 getDao() {
-            return dao;
-        }
+    @Override
+    protected ClienteDAO2 getDao() {
+        return dao;
+    }
 
-//	@Autowired
-//	private ClienteDAO clienteDao;
-//
-//	private List<Cliente> clienteList;
-//
-//
-//
-//
-//	@RequestMapping(value = "cadastrocliente", method = RequestMethod.GET)
-//	public ModelAndView cadastroCliente() {
-//
-//		ModelAndView cadastrocliente = new ModelAndView("cadastrocliente");
-//
-//		return cadastrocliente;
-//	}
-//
-//	@RequestMapping(value = "/AddCliente", method = RequestMethod.POST)
-//	public ModelAndView AdicionarCliente(@ModelAttribute("Cliente") Cliente cliente) {
-//
-//		ModelAndView cadastro_cliente = new ModelAndView("cadastrocliente");
-//
-//		clienteDao.add(cliente);
-//
-//		return cadastro_cliente;
-//	}
-//
-//
-//
-//	@RequestMapping(value = "/movimentacaocliente", method = RequestMethod.GET)
-//	public ModelAndView movimentacaoCliente() {
-//
-//		ModelAndView movimentacaocliente = new ModelAndView("movimentacaocliente");
-//
-//		clienteList = clienteDao.getAll();
-//
-//		movimentacaocliente.addObject("clienteList",clienteList);
-//
-//		return movimentacaocliente;
-//	}
-//
-//
-//	@RequestMapping(value = "/edicaocliente", method = RequestMethod.GET)
-//	public ModelAndView editarClienteForm(HttpServletRequest request) {
-//
-//		Cliente cliente;
-//		long idf = Long.parseLong(request.getParameter("id"));
-//		ModelAndView edicaocliente = new ModelAndView("edicaocliente");
-//		cliente = clienteDao.PegarPorId(idf);
-//		edicaocliente.addObject("cliente", cliente);
-//
-//
-//		return edicaocliente;
-//	}
-//
-//
-//	@RequestMapping(value = "/movimentacaocliente/edicaocliente", method = RequestMethod.POST)
-//	public ModelAndView editarCliente(HttpServletRequest request, Cliente cliente) {
-//
-//
-//
-//		Long idf = Long.parseLong(request.getParameter("id"));
-//		clienteDao.editar(idf, cliente);
-//
-//
-//		return new ModelAndView("redirect:/movimentacaocliente");
-//	}
-//
-//
-//
-//	@RequestMapping(value = "/movimentacaocliente/delete", method = RequestMethod.GET)
-//	public ModelAndView deleteCliente(HttpServletRequest request) {
-//
-//
-//		long idf = Long.parseLong(request.getParameter("id"));
-////		ModelAndView movimentacaocategoria = new ModelAndView("movimentacaocategoria");
-//		clienteDao.delete(idf);
-//
-//
-//		return new ModelAndView("redirect:/movimentacaocliente");
-//	}
 
 }
