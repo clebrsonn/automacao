55d0745429098fcb92ad0e21bf8567ae6ce5be5e
windsonleo <windsor_melo@hotmail.com>
2016-08-02 13:58:40 -0300

    repository

--- src/main/java/com/teccsoluction/sushi/dao/ReservaDAO.java
+++ src/main/java/com/teccsoluction/sushi/dao/ReservaDAO.java
@@ -11,7 +11,8 @@ import org.springframework.transaction.annotation.Transactional;
 import com.teccsoluction.sushi.entidade.Reserva;
 
 
-@Service
+
+@Repository
 public class ReservaDAO {
 	
 	@PersistenceContext
