cffd2524a0a87a770545485a13d9c6b195752418
Cleberson <clebrsonn@hotmail.com>
2016-08-19 10:19:52 -0300

    Update PedidoVenda.java

--- src/main/java/com/teccsoluction/sushi/entidade/PedidoVenda.java
+++ src/main/java/com/teccsoluction/sushi/entidade/PedidoVenda.java
@@ -37,7 +37,7 @@ public class PedidoVenda extends Pedido {
 
 
 
-	//lista de devoluções de compra
+	//lista de devolucoes de compra
     @OneToMany(mappedBy="pedidoVenda")
     private List<DevolucaoVenda> listaDevolucao;
     
@@ -45,7 +45,7 @@ public class PedidoVenda extends Pedido {
     
     
 
-    //CONSTRUTOR PADRÃO
+    //CONSTRUTOR PADRAO
     public PedidoVenda() {
     	
     	listaDevolucao = new ArrayList<>(); 
