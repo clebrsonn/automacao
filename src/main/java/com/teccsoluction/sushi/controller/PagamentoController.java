51861544411a7124c287ab27ff4156772e3d22c4
cleberson <clebrsonn@hotmail.com>
2017-01-08 12:14:29 -0300

    correções pacote

--- src/main/java/com/teccsoluction/sushi/controller/PagamentoController.java
+++ src/main/java/com/teccsoluction/sushi/controller/PagamentoController.java
@@ -1,6 +1,6 @@
 package com.teccsoluction.sushi.controller;
 
-import com.teccsoluction.sushi.dao.generic.PagamentoDAO;
+import com.teccsoluction.sushi.dao.PagamentoDAO;
 import com.teccsoluction.sushi.entidade.Pagamento;
 import com.teccsoluction.sushi.framework.AbstractController;
 import org.springframework.beans.factory.annotation.Autowired;
